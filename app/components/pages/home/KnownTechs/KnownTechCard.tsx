e8c66c6236799019ba6f8d6c27c29e4c1a93ebcf
Gabriel Nogueira <bbynogueira@gmail.com>
2023-10-26 19:19:33 -0300

    Removed transition

--- app/components/pages/home/KnownTechs/KnownTechCard.tsx
+++ app/components/pages/home/KnownTechs/KnownTechCard.tsx
@@ -18,7 +18,7 @@ export const KnownTechCard = ({
   );
 
   return (
-    <div className='flex flex-col gap-2 rounded-lg bg-gray-600/20 p-6 text-gray-500 transition-all hover:bg-gray-600/30 hover:text-purple-400 hover:shadow-low'>
+    <div className='flex flex-col gap-2 rounded-lg bg-gray-600/20 p-6 text-gray-500 hover:bg-gray-600/30 hover:text-purple-400 hover:shadow-low'>
       <div className='flex items-center justify-between text-purple-400'>
         <p className='font-medium'>{name}</p>
         {icon}
